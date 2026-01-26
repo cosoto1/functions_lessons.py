2d8dc3f378ba50761c1e623c3eca7d0652c409dc
cosoto1 <Cosoto1@cps.edu>
2026-01-26 18:46:32 +0000

    Functions

--- 1_.py
+++ 1_.py
@@ -1,3 +1,56 @@
+# A function = a block of reuable code
+    # place a () after the fuction
+
+# def happy_birthday(name, age):
+#     Print("Happy birthday to you (name)!")
+#     Print("You are (age) are you")
+#     Print("Happy birthday to you")
+#     Print()
+
+# happy_birthday("Bro", 20)
+# happy_birthday("Steve", 30)
+# happy_birthday("Joe", 40)
+
+# def display_inverse(username, amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"You bill of ${amount:.2f} is due: {due_date} ")
+
+# display_inverse("BroCode", 42.50, "01/01")
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def miltiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add (1,2))
+# print(subtract (1,2))
+# print(miltiply (1,2))
+# print(divide (1,2))
+
+
+def create_name(first, last):
+    first = first.capitialize()
+    last = last.capitialize()
+    return first + " " + last
+
+full_name = create_name("Spongebob","Squarpants")
+print(full_name)
+
+
+
+
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
